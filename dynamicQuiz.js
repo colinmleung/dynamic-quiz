54e3328bce6a9bebb768bc310351094e1dcc157e
Colin Leung <colinmleung@gmail.com>
2014-10-18 02:40:09 -0700

    Added back button behaviour

--- dynamicQuiz.js
+++ dynamicQuiz.js
@@ -2,45 +2,68 @@ $("document").ready(function(){
 	//questions and answers
 	var allQuestions = [{question:"Who is Prime Minister of the United Kingdom?", choices:["David Cameron", "Gordon Brown", "Winston Churchill", "Tony Blair"], correctAnswer: 0},{question:"Question2", choices:["Choice1", "Choice2", "Choice3"], correctAnswer: 2}];
 
-	var score = 0;
+  var quizAnswers = [];
 	var questionIndex = 0;
 
 	//show first question
-	setUpQuestions(allQuestions, questionIndex);
+	setQuizState(questionIndex, allQuestions[questionIndex], quizAnswers[questionIndex]);
 
 	$("#next").click(function() {
 
     // validate radio buttons first
     $("#error").text("");
-		var userAnswer = $("form input[type='radio']:checked").val();
-    if (userAnswer) {
-      if (userAnswer == allQuestions[questionIndex].correctAnswer) {
-        score++;
-      }
-
-      // display the next question or display the score
+		quizAnswers[questionIndex] = $("form input[type='radio']:checked").val();
+    if (quizAnswers[questionIndex]) {
       questionIndex++;
-      if (questionIndex == allQuestions.length) {
-        $("#next").hide();
-        $("#question").hide();
-        $("#choices").empty();
-        $("#score").text("Your score is: " + score);
-      } else {
-        setUpQuestions(allQuestions, questionIndex);
-      }
+      setQuizState(questionIndex, allQuestions[questionIndex], quizAnswers[questionIndex]);
     } else {
       $("#error").text("Please choose an answer.");
-    }
-		// score the question
+    }// score the question
 		
 	});
 
-	function setUpQuestions(allQuestions, questionIndex) {
-		$("#question").text(allQuestions[questionIndex].question);
-		$("#choices").empty();
-		for (var i = 0; i < allQuestions[questionIndex].choices.length; i++) {
-			$("<input type='radio' name='question' value="+i+">"+allQuestions[questionIndex].choices[i]+"<br>").appendTo("#choices");
+  $("#back").click(function() {
+    questionIndex--;
+    setQuizState(questionIndex, allQuestions[questionIndex], quizAnswers[questionIndex]);
+  });
 
-		}
-	}
+  function setQuizState(questionIndex, questionObject, answer) {
+    cleanState();
+    if (questionIndex !== 0) {
+      $("#back").show();
+    }
+    if (questionIndex !== allQuestions.length) {
+      $("#next").show();
+      $("#question").text(questionObject.question);
+      for (var i = 0; i < questionObject.choices.length; i++) {
+        if (i == answer) {
+          $("<input type='radio' name='question' value="+i+" checked>"+allQuestions[questionIndex].choices[i]+"<br>").appendTo("#choices");
+              } else {
+        $("<input type='radio' name='question' value="+i+">"+allQuestions[questionIndex].choices[i]+"<br>").appendTo("#choices");
+              }
+      }
+    } else {
+      var score = calculateScore();
+      $("#score").text("Your score is: " + score);
+    }
+  }
+
+  function cleanState() {
+    $("#question").text("");
+    $("#choices").empty();
+    $("#score").text("");
+    $("#error").text("");
+    $("#next").hide();
+    $("#back").hide();
+  }
+
+  function calculateScore() {
+    var score = 0;
+    for (var i = 0; i < allQuestions.length; i++) {
+      if (allQuestions[i].correctAnswer == quizAnswers[i]) {
+        score++;
+      }
+    }
+    return score;
+  }
 });
