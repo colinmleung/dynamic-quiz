c8cc8cf5a67c43d2262ce00038f06a854f1e8ecd
Colin Leung <colinmleung@gmail.com>
2014-10-17 23:41:24 -0700

    Initial version of dynamic quiz

---
+++ dynamicQuiz.js
@@ -0,0 +1,38 @@
+$("document").ready(function(){
+	//questions and answers
+	var allQuestions = [{question:"Who is Prime Minister of the United Kingdom?", choices:["David Cameron", "Gordon Brown", "Winston Churchill", "Tony Blair"], correctAnswer: 0},{question:"Question2", choices:["Choice1", "Choice2", "Choice3"], correctAnswer: 2}];
+
+	var score = 0;
+	var questionIndex = 0;
+
+	//show first question
+	setUpQuestions(allQuestions, questionIndex);
+
+	$("#next").click(function() {
+		// score the question
+		var userAnswer = $("form input[type='radio']:checked").val();
+		if (userAnswer == allQuestions[questionIndex].correctAnswer) {
+			score++;
+		}
+
+		// display the next question or display the score
+		questionIndex++;
+		if (questionIndex == allQuestions.length) {
+			$("#next").hide();
+			$("#question").hide();
+			$("#choices").empty();
+			$("#score").text("Your score is: " + score);
+		} else {
+			setUpQuestions(allQuestions, questionIndex);
+		}
+	});
+
+	function setUpQuestions(allQuestions, questionIndex) {
+		$("#question").text(allQuestions[questionIndex].question);
+		$("#choices").empty();
+		for (var i = 0; i < allQuestions[questionIndex].choices.length; i++) {
+			$("<input type='radio' name='question' value="+i+">"+allQuestions[questionIndex].choices[i]+"<br>").appendTo("#choices");
+
+		}
+	}
+});
